23cdff0baa054763061faf36b2a12bf4006252a4
AwokeInAnEnigma <kurogames7595@protonmail.com>
2022-02-01 23:02:32 -0500

    Add project files.

---
+++ Modules/Commands.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using Discord;
+using Discord.Commands;
+using Discord.WebSocket;
+
+namespace TutorialBot.Modules
+{
+    public class Commands : ModuleBase<SocketCommandContext>
+    {
+        [Command("send")]
+        public async Task SND([Remainder] string reason = null)
+        {
+            //CHANGE THESE
+            var ch = Context.Client.GetGuild(000).GetTextChannel(999);
+            string filtered = FilterRetardation(reason);
+            var bytes = new byte[3];
+            using (var rng = new RNGCryptoServiceProvider())
+            {
+                rng.GetBytes(bytes);
+            }
+
+            // and if you need it as a string...
+            string hash1 = BitConverter.ToString(bytes);
+
+            var EmbedBuilderLog = new EmbedBuilder()
+       .WithDescription($"Anonymous Message: {filtered} -- HASH: {hash1}")
+       .WithImageUrl("https://i.imgur.com/hi57MqN.gif")
+       .WithFooter(footer =>
+       {
+           footer
+           .WithIconUrl("https://i.imgur.com/hi57MqN.gif");
+       });
+            var built = EmbedBuilderLog.Build();
+            await ch.SendMessageAsync(embed: built);
+          //  await ReplyAsync(reason);
+        }
+            
+        public string FilterRetardation(string msg) {
+            //...ugh
+
+            msg = Regex.Replace(msg, "nigger", "------", RegexOptions.IgnoreCase);
+            msg = Regex.Replace(msg, "n1gg3r", "------", RegexOptions.IgnoreCase);
+            msg = Regex.Replace(msg, "nigg3r", "------", RegexOptions.IgnoreCase);
+            msg = Regex.Replace(msg, "n1gger", "------", RegexOptions.IgnoreCase);
+            msg = Regex.Replace(msg, "tranny", "------", RegexOptions.IgnoreCase);
+            msg = Regex.Replace(msg, "tr4nny", "------", RegexOptions.IgnoreCase);
+            msg = Regex.Replace(msg, "faggot", "------", RegexOptions.IgnoreCase);
+            msg = Regex.Replace(msg, "f4gg0t", "------", RegexOptions.IgnoreCase);
+            msg = Regex.Replace(msg, "fag", "---", RegexOptions.IgnoreCase);
+            msg = Regex.Replace(msg, "f4g", "---", RegexOptions.IgnoreCase);
+            msg = Regex.Replace(msg, "kike", "----", RegexOptions.IgnoreCase);
+            msg = Regex.Replace(msg, "k1ke", "----", RegexOptions.IgnoreCase);
+            msg = Regex.Replace(msg, "trannie", "-------", RegexOptions.IgnoreCase);
+            msg = Regex.Replace(msg, "tr4nni3", "-------", RegexOptions.IgnoreCase);
+            msg = Regex.Replace(msg, "you will never be a real woman", "( i have no bitches or social life )", RegexOptions.IgnoreCase);
+            msg = Regex.Replace(msg, "you will never be a real man", "( i have no bitches or social life )", RegexOptions.IgnoreCase);
+            msg = Regex.Replace(msg, "AMBER YOU WILL NEVER BE A REAL WOMAN NO ONE WILL EVER LOVE YOU", "( i have no bitches or social life )", RegexOptions.IgnoreCase);
+            msg = Regex.Replace(msg, "AMBER YOU WILL NEVER BE A REAL WOMAN NO ONE WILL EVER LOVE YOU", "( i should kill myself now )", RegexOptions.IgnoreCase);
+            msg = Regex.Replace(msg, "real man", "==(POSTER IS A BITCH)==", RegexOptions.IgnoreCase);
+            msg = Regex.Replace(msg, "real woman", "==(POSTER IS A BITCH)==", RegexOptions.IgnoreCase);
+
+            msg = Regex.Replace(msg, "n.igger", "-------", RegexOptions.IgnoreCase);
+            msg = Regex.Replace(msg, "n.i.gger", "-------", RegexOptions.IgnoreCase);
+            msg = Regex.Replace(msg, "n.i.g.ger", "-------", RegexOptions.IgnoreCase);
+            msg = Regex.Replace(msg, "n.i.g.g.er", "-------", RegexOptions.IgnoreCase);
+            msg = Regex.Replace(msg, "n.i.g.g.e.r", "------", RegexOptions.IgnoreCase);
+           
+            msg = Regex.Replace(msg, "ni.gger", "-------", RegexOptions.IgnoreCase);
+            msg = Regex.Replace(msg, "nig.ger", "-------", RegexOptions.IgnoreCase);
+            msg = Regex.Replace(msg, "nigg.er", "-------", RegexOptions.IgnoreCase);
+            msg = Regex.Replace(msg, "nigge.r", "-------", RegexOptions.IgnoreCase);
+            msg = Regex.Replace(msg, "nigger", "-------", RegexOptions.IgnoreCase);
+            msg = Regex.Replace(msg, "n.igger", "-------", RegexOptions.IgnoreCase);
+            return msg;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,111 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using Discord;
+using Discord.Commands;
+using Discord.WebSocket;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace TutorialBot
+{
+    class Program
+    {
+        static void Main(string[] args) => new Program().RunBotAsync().GetAwaiter().GetResult();
+
+        private DiscordSocketClient _client;
+        private CommandService _commands;
+        private IServiceProvider _services;
+        private StreamWriter wroter;
+
+        public async Task RunBotAsync()
+        {
+            _client = new DiscordSocketClient();
+            _commands = new CommandService();
+
+            _services = new ServiceCollection()
+                .AddSingleton(_client)
+                .AddSingleton(_commands)
+                .BuildServiceProvider();
+
+            string token = "MYBOTTOKENHERE";
+
+            _client.Log += _client_Log;
+
+            await RegisterCommandsAsync();
+
+            await _client.LoginAsync(TokenType.Bot, token);
+
+            await _client.StartAsync();
+
+            await Task.Delay(-1);
+
+        }
+
+        private Task _client_Log(LogMessage arg)
+        {
+            Console.WriteLine(arg);
+            return Task.CompletedTask;
+        }
+            
+        public async Task RegisterCommandsAsync()
+        {
+
+            _client.MessageReceived += HandleCommandAsync;
+            await _commands.AddModulesAsync(Assembly.GetEntryAssembly(), _services);    
+        }
+
+        public List<string> duh = new List<string>();
+        public bool firstTimeFill = false;
+        private async Task SendAMessage(SocketMessage arg, SocketUserMessage message, SocketCommandContext context) {
+            if (firstTimeFill == false)
+            {
+                foreach (string line in System.IO.File.ReadLines(KnownFolders.GetPath(KnownFolders.KnownFolder.Documents) + "\\DiscordMessages.txt"))
+                {
+                    duh.Add(line);
+                    firstTimeFill = true;
+                }
+            }
+
+            //var channel = _client.GetGuild(3).GetChannel(4);
+            if (wroter == null || wroter.BaseStream == null) {
+                wroter = new StreamWriter(KnownFolders.GetPath(KnownFolders.KnownFolder.Documents) + "\\wuep.txt");
+            }
+            int argPos = 0;
+            if (message.HasStringPrefix("__", ref argPos))
+            {
+                duh.Add($"Content: {message.Content} -- Author: { message.Author} -- Time: {message.Timestamp.DateTime.ToLocalTime() }");
+                Console.WriteLine($"Content: {message.Content} -- Author: { message.Author}");
+                var result = await _commands.ExecuteAsync(context, argPos, _services);
+                if (!result.IsSuccess) Console.WriteLine(result.ErrorReason);
+                if (result.Error.Equals(CommandError.UnmetPrecondition)) await message.Channel.SendMessageAsync(result.ErrorReason);
+            }
+
+            TextWriter tw = new StreamWriter(KnownFolders.GetPath(KnownFolders.KnownFolder.Documents) + "\\DiscordMessages.txt");
+
+            foreach (string s in duh)
+                tw.WriteLine(s);
+
+            tw.Close();
+            //System.IO.File.WriteAllLines(KnownFolders.GetPath(KnownFolders.KnownFolder.Documents) + "\\wuep.txt", duh);
+        }
+
+        private async Task HandleCommandAsync(SocketMessage arg)
+        {
+            var message = arg as SocketUserMessage;
+            var context = new SocketCommandContext(_client, message);
+            if (message.Author.IsBot) return;
+
+            int argPos = 0;
+
+            if (message.Channel is IPrivateChannel) {
+                await SendAMessage(arg, message, context);
+            //    await context.User.SendMessageAsync("CuMMIN");
+            }
+
+        }
+    }
+}
